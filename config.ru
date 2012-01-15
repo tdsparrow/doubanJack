6d8b29b30f260cdffe242b4805e18c316e50a09e
Honnix Liang <hxliang1982@gmail.com>
2012-01-15 22:36:49 +0800

    encode with UTF-16BE, textenc must be greater than 0, otherwise all string will be treated as plain ASCII characters also clean code

--- config.ru
+++ config.ru
@@ -13,7 +13,7 @@ configure :development do
       super
     end
   end
-  
+
   use Sinatra::Reloader
 end
 
--- douban.rb
+++ douban.rb
@@ -10,13 +10,12 @@ set :app_file, __FILE__
 get '/*' do
   halt 404 unless request.env['REQUEST_URI'] =~ /douban\.(fm|com)/
   uri, ret = getURI request
-  
+
   process( uri, ret ) if ret.status[0] == '200'
 
   status ret.status[0]
   headers ret.meta
   body ret
-  
 end
 
 def process(uri, ret)
@@ -37,7 +36,6 @@ def processJson(meta)
   meta.rewind
 end
 
-
 def processCover(filename, ret) 
   file = "library/.meta/#{filename}"
   dumpResource(file, ret)
@@ -54,16 +52,15 @@ def dumpResource(file, ret)
   File.open(file, 'w') { |f| f.write ret.read; ret.rewind } if StringIO === ret
 end
 
-
 def conv str
-  str.encode("gb18030", "UTF-8")
+  str.encode("UTF-16BE", "UTF-8")
 end
 
 def fillUpTags(file)
   file =~ /([0-9]*)\.mp3/
   if File.exist? "library/.meta/#{$1}.json"
     j = JSON.parse File.read("library/.meta/#{$1}.json")
-    
+
     pic =  "library/.meta/#{File.basename(j['picture'])}"
     if not File.exist? pic
       processCover(File.basename(j['picture']), open(j['picture'].sub 'mpic', 'lpic'))
@@ -72,21 +69,19 @@ def fillUpTags(file)
     begin
       tag = ID3Lib::Tag.new file, ID3Lib::V2
       tag.strip!
-      tag << {:id => :TIT2, :text => conv(j['title']), :textenc => 0 }
-      tag << {:id => :TALB, :text => conv(j['albumtitle']), :textenc => 0 }
-      tag << {:id => :TPUB, :text => conv(j['company']), :textenc => 0 }
-      tag << {:id => :TPE1, :text => conv(j['artist']), :textenc => 0 }
-      cover = { :id => :APIC, :mimetype => 'image/jpeg', :picturetype => 3, :data => File.read(pic) }
+      tag << {:id => :TIT2, :text => conv(j['title']), :textenc => 1}
+      tag << {:id => :TALB, :text => conv(j['albumtitle']), :textenc => 1}
+      tag << {:id => :TPUB, :text => conv(j['company']), :textenc => 1}
+      tag << {:id => :TPE1, :text => conv(j['artist']), :textenc => 1}
+      cover = {:id => :APIC, :mimetype => 'image/jpeg', :picturetype => 3, :data => File.read(pic)}
       tag << cover
       tag.update!
 
       File.rename file, "library/#{j['title'].gsub(/\s/, '_')}.mp3"
     rescue Iconv::IllegalSequence => conv
     end  
-    
   end
 end
-
 
 def getURI request
   uri = URI.escape(URI.unescape(request.env["REQUEST_URI"]))
@@ -97,10 +92,9 @@ def getURI request
   rescue OpenURI::HTTPError => err
     ret = err.io
   end
-  
-  [ uri, ret ]
-end
 
+  [uri, ret]
+end
 
 def header req
   new_head = {}
